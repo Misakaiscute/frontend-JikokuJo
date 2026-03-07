56205953bec560d8eddb14428fac545eaf23a078
Misakaiscute <horchy.hunorpihe@gmail.com>
2026-03-07 15:37:23 +0100

    basic map added

---
+++ src/lib/map/Map.svelte
@@ -0,0 +1,11 @@
+﻿<script lang="ts">
+    import Map from "./map.svelte.ts";
+    import {onMount} from "svelte";
+
+    let mapElement: HTMLDivElement;
+
+    onMount(() => {
+        const map = new Map(mapElement);
+    });
+</script>
+<div bind:this={mapElement} class="h-svh w-svw"></div>
---
+++ src/lib/map/map.svelte.ts
@@ -0,0 +1,15 @@
+﻿import Leaflet from "leaflet";
+
+export default class Map{
+    private readonly map: Leaflet.Map | undefined;
+
+    constructor(mapHolderId: HTMLDivElement) {
+        this.map = Leaflet.map(mapHolderId)
+           .setView([47.49031325038684, 19.053912410022516], 10);
+
+        Leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>"
+            }
+        ).addTo(this.map);
+    }
+}
