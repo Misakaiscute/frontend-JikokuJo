fb7587ccaf1f4149318c778f79bc279d8422ac15
Hunor Horchy <horchy.hunorpihe@gmail.com>
2026-05-09 13:50:51 +0200

    Fixed logout button color

--- src/lib/profile/presentation/account/AccountPanel.svelte
+++ src/lib/profile/presentation/account/AccountPanel.svelte
@@ -63,8 +63,8 @@
 {/snippet}
 {#snippet logout()}
     {#snippet logoutIcon()}
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"class="flex-[0_0_75%] aspect-square stroke-red-800">
-            <path d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h6q.425 0 .713.288T12 4t-.288.713T11 5H5v14h6q.425 0 .713.288T12 20t-.288.713T11 21zm12.175-8H10q-.425 0-.712-.288T9 12t.288-.712T10 11h7.175L15.3 9.125q-.275-.275-.275-.675t.275-.7t.7-.313t.725.288L20.3 11.3q.3.3.3.7t-.3.7l-3.575 3.575q-.3.3-.712.288t-.713-.313q-.275-.3-.262-.712t.287-.688z" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="flex-[0_0_75%] aspect-square stroke-red-800 fill-red-800">
+            <path d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h6q.425 0 .713.288T12 4t-.288.713T11 5H5v14h6q.425 0 .713.288T12 20t-.288.713T11 21zm12.175-8H10q-.425 0-.712-.288T9 12t.288-.712T10 11h7.175L15.3 9.125q-.275-.275-.275-.675t.275-.7t.7-.313t.725.288L20.3 11.3q.3.3.3.7t-.3.7l-3.575 3.575q-.3.3-.712.288t-.713-.313q-.275-.3-.262-.712t.287-.688z"/>
         </svg>
     {/snippet}
     <div class="flex-[0_0_auto] w-full pb-2 px-3.5 mt-auto flex justify-end items-center">
